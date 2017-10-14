ba394b600cd920e1e0b0c0951bc658cd30e99edc
MustafaBKLZ <mustafabukulmez3446>
2017-10-14 13:34:07 +0300

    sitemenu

---
+++ MVCBlog/Areas/Admin/Controllers/SiteMenuController.cs
@@ -0,0 +1,44 @@
+﻿using MVCBlog.Areas.Admin.Models.DTO;
+using MVCBlog.Models.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Areas.Admin.Controllers
+{
+    
+    public class SiteMenuController : BaseController
+    {
+        
+        public ActionResult AddSiteMenu()
+        {
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult AddSiteMenu(SiteMenuVM model)
+        {
+            SiteMenu sitemenu = new SiteMenu();
+
+            sitemenu.smn_adi = model.smn_adi;
+            sitemenu.smn_url = model.smn_url;
+            sitemenu.cssClass = model.cssClass;
+
+            sitemenu.Kaydeden = 1;
+            sitemenu.Aktif = true;
+            sitemenu.KayitTarihi = DateTime.Now;
+
+            db.SiteMenus.Add(sitemenu);
+            db.SaveChanges();
+
+            ViewBag.IslemDurum = 1;
+
+            return View();
+        }
+    }
+}
---
+++ MVCBlog/Areas/Admin/Models/DTO/SiteMenuVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Areas.Admin.Models.DTO
+{
+    public class SiteMenuVM : BaseVM
+    {
+
+        public string smn_adi { get; set; }
+        public string smn_url { get; set; }
+        public string cssClass { get; set; }
+
+    }
+}
