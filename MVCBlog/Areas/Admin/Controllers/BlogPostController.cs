a9884820d74f428211999329519cd08e3d23fd9d
MustafaBKLZ <mustafabukulmez3446>
2017-10-16 15:06:41 +0300

    ck editor

---
+++ MVCBlog/Areas/Admin/Controllers/BlogPostController.cs
@@ -0,0 +1,49 @@
+﻿using MVCBlog.Areas.Admin.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Areas.Admin.Controllers
+{
+    public class BlogPostController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddBlogPost()
+        {
+            BlogPostVM model = new BlogPostVM();
+            model.drpKategoriler = db.Kategoriler.Select(x => new SelectListItem()
+            {
+                Text = x.kat_Adi,
+                Value = x.ID.ToString()
+            }).ToList();
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddBlogPost(BlogPostVM model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                ViewBag.IslemDurumu = 1;
+                return View();
+            }
+            else
+            {
+
+                ViewBag.IslemDurumu = 1;
+                return View();
+            }
+
+
+        }
+    }
+}
---
+++ MVCBlog/Areas/Admin/Models/DTO/BlogPostVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Areas.Admin.Models.DTO
+{
+    public class BlogPostVM : BaseVM
+    {
+        public string blg_Baslik { get; set; }
+        public string blg_Icerik { get; set; }
+        [Required]
+        public int kat_ID { get; set; }
+        public IEnumerable<SelectListItem> drpKategoriler { get; set; }
+
+    }
+}
